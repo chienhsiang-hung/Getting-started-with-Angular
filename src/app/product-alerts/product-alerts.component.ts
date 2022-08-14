7626a9e76e37648d2129074b53728f0759176500
chienhsiang-hung <abcdefg2981@gmail.com>
2022-08-14 20:05:04 +0800

    1 file modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
